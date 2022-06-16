02b70c045120eb401119f108e5ec0852c9d5c27e
Hikari7 <h.kobe712@gmail.com>
2022-06-15 22:46:02 -0700

    modified

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 const a_up = document.querySelector(".a_up");
 const a_reset = document.querySelector(".a_reset");
-const a_player = document.querySelector(".a_player");
+let a_player = document.querySelector(".a_player");
 const b_up = document.querySelector(".b_up");
 const b_reset = document.querySelector(".b_reset");
-const b_player = document.querySelector(".b_player");
+let b_player = document.querySelector(".b_player");
 
-let aScore = 0;
-let bScore = 0;
+
+let scoreBoard = {
+   aScore: 0,
+   bScore: 0,
+   winningScore: null
+}
 
 function countA() {
   a_up.addEventListener("click", () => {
-    aScore++;
-    console.log(a_up);
-    a_player.textContent = aScore;
+    scoreBoard.aScore++;
+    a_player.textContent =scoreBoard.aScore;
   });
 
   a_reset.addEventListener("click", () => {
@@ -24,9 +27,8 @@ countA();
 
 function countB() {
   b_up.addEventListener("click", () => {
-    bScore++;
-    console.log(a_up);
-    b_player.textContent = bScore;
+    scoreBoard.bScore++;
+    b_player.textContent = scoreBoard.bScore;
   });
 
   b_reset.addEventListener("click", () => {
@@ -41,25 +43,34 @@ countB();
 const scoreSet = document.querySelector(".score_setting");
 const menuBtn = document.querySelector(".dropdown_menu");
 const menuItems = document.querySelector(".dropdown_items");
-const item = document.querySelectorAll(".dropdown_item");
+const item = document.querySelector(".dropdown_item");
+
+const item21 = document.querySelector ("dropdown_item21");
 
 function openItems() {
   //when you click the score menu
   menuBtn.addEventListener("click", () => {
-    // item.classList.add(".open");
-    // menuBtn.classList.add("hidden");
+    //メニュー一覧をtoggle
     menuItems.classList.toggle("active");
-
-    //querySelectorAllを配列に変える
-    // const convertedArr = Array.from(item);
-    // convertedArr.forEach((items) => {
-    //   console.log(item);
-    //   item.addEventListener("click", () => {
-    //     item.parentNode.classList.toggle("active");
-    //   });
-
-    // });
   });
+}
+
+
+
+function selectScore() {
+  // 各itemのquerySelectorAllを配列に変える
+  // const convertedArr = Array.from(item);
+  // convertedArr.forEach((el) => {
+    // item.addEventListener("click", (e) => {
+    //   scoreBoard.winningScore = parseInt(item.item);
+
+    //   console.log(scoreBoard.winningScore);
+    // });
+  // });
+
+  // item21.addEventListener('click', () => {
+  //   console.log("got the value");
+  // })
 }
 
 openItems();
